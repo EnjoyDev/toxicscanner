6569d58255e85182408d308048fdd695ad1af685
max <maxime.berthou@gmail.com>
2021-07-06 21:39:03 +0200

    déplacement de la liste des produits dans un singleton

---
+++ toxicscanner-master/app/src/main/java/com/blackfrogweb/toxicscanner/ProductList.kt
@@ -0,0 +1,60 @@
+package com.blackfrogweb.toxicscanner
+
+import android.content.Context
+import android.content.res.Resources
+import com.opencsv.CSVReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.*
+
+class ProductList private constructor(context: Context)
+{
+    val list : List<ProductData> = buildProductList(context)
+
+    companion object {
+        private var instance: ProductList? = null
+        fun getInstance(context: Context): ProductList {
+            if (instance == null) {
+                instance = ProductList(context)
+            }
+
+            return instance!!
+        }
+    }
+
+    fun searchProduct( barcode : String) : ProductData?
+    {
+        return list.find { pdt -> pdt.barcode == barcode}
+    }
+
+    private fun buildProductList(context : Context) : List<ProductData>
+    {
+        var streamReader: InputStreamReader? = null
+        var csvReader: CSVReader? = null
+        val productList : MutableList<ProductData> = mutableListOf()
+        try
+        {
+            streamReader = InputStreamReader(context.assets?.open("toxic.csv"))
+            csvReader = CSVReader(streamReader)
+            var record = csvReader.readNext()
+            while ( record != null)
+            {
+                if(record.size >= 5)
+                {
+                    val pdtData = ProductData(record[0], record[1],  record[2], record[3], record[4])
+                    productList.add(pdtData)
+                }
+                record = csvReader.readNext()
+            }
+        }
+        catch (e: IOException)
+        {
+            e.printStackTrace()
+        }
+        finally {
+            csvReader!!.close()
+            streamReader!!.close()
+        }
+        return productList.toList()
+    }
+}
--- toxicscanner-master/app/src/main/java/com/blackfrogweb/toxicscanner/ProductListViewModel.kt
+++ toxicscanner-master/app/src/main/java/com/blackfrogweb/toxicscanner/ProductListViewModel.kt
@@ -2,51 +2,8 @@ package com.blackfrogweb.toxicscanner
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import java.io.IOException
-import java.io.InputStreamReader
-import java.util.*
-import com.opencsv.CSVReader
 
 class ProductListViewModel(application: Application) : AndroidViewModel(application)
 {
-    private fun buildProductList() : List<ProductData>
-    {
-        var streamReader: InputStreamReader? = null
-        var csvReader: CSVReader? = null
-        val productList = Vector<ProductData>(10000)
-        try
-        {
-            streamReader = InputStreamReader(getApplication<Application>().assets?.open("toxic.csv"))
-            csvReader = CSVReader(streamReader)
-            var record = csvReader.readNext()
-            while ( record != null)
-            {
-                if(record.size >= 5)
-                {
-                    val pdtData = ProductData(record[0], record[1],  record[2], record[3], record[4])
-                    productList.add(pdtData)
-                }
-                record = csvReader.readNext()
-            }
-            csvReader.close()
-        }
-        catch (e: IOException)
-        {
-            e.printStackTrace()
-        }
-        finally {
-            csvReader!!.close()
-            streamReader!!.close()
-        }
-        return productList.toList()
-    }
-
-    fun searchProduct( barcode : String) : ProductData?
-    {
-        return productList.find { pdt -> pdt.barcode == barcode}
-    }
-
-    private val productList : List<ProductData> = buildProductList()
-
-    val productAdapter : ProductItemAdapter = ProductItemAdapter(application, productList)
+    val productAdapter : ProductItemAdapter = ProductItemAdapter(application, ProductList.getInstance(application.applicationContext).list)
 }
